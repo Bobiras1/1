925c24c9a4ac794a2e4e4db86292f7d2e787b7ab
Bobiras1 <trelakourgiou@gmail.com>
2025-07-16 12:01:57 +0300

    Create 1.swift

---
+++ 1.swift
@@ -0,0 +1,6 @@
+//Setting Andromeda =1 Andros=1 Crete = 1 Gemma = 1 Gibberlink_mode=1 we have
+//print("E = Andromeda*Andros*Crete*Gemma*Gibberlink_mode*mcmod2")
+//You can write it also as a comment
+//print("E = Andromeda*Andros*Crete*Gemma*Gibberlink_mode*mcmod2")
+//or you could play setting Shakespeare= 1
+print("E = Shakespeare*mcmod2")
